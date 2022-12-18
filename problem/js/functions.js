91af010cde90c8f7d3fdac08e593709b6281bedb
IsakMakra <isak.makra01@gmail.com>
2022-12-18 21:18:08 +0100

    Wrote the function

--- problem/js/functions.js
+++ problem/js/functions.js
@@ -48,6 +48,12 @@ function create_filter_element (data) {
       Returns a reference to the new dom-element
   */
 
+    let dom = document.createElement("li");
+    dom.classList.add(data.class);
+    document.querySelector(data.parent).appendChild(dom);
+    dom.textContent = data.textContent;
+    dom.addEventListener("click", click_filter_element);
+    return dom;
 }
 
 
